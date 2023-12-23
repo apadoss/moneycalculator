9b57387b70e310fa44346558fd6ac2836bdb9132
Alejandro <apadoss03@gmail.com>
2023-12-23 20:27:10 +0000

    MockMoneyDisplay class created

--- src/main/java/software/ulpgc/moneycalculator/Currency.java
+++ src/main/java/software/ulpgc/moneycalculator/Currency.java
@@ -1,5 +1,8 @@
 package software.ulpgc.moneycalculator;
 
 public record Currency(String code, String name) {
-
+    @Override
+    public String toString() {
+        return code + " " + name;
+    }
 }
